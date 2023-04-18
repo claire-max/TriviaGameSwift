992561a10cf3b729768fa016c3db81361e21e59b
ClaireFree <claire@robingoodfoods.com>
2023-04-18 13:44:54 -0700

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Claire Freeman on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
